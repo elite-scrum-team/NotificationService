a020bdf70fe4146d07cd68649f572b2d92a0ba9a
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-11 15:02:26 +0100

    fix

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,49 +1,66 @@
 const router = require('express').Router();
 const controller = require('../controllers/emailController');
 
-
 router.post('/register', async (req, res) => {
-  const payload = req.body.payload;
-  if (payload.email) {
-    await res.send(
-      await controller.sendRegistrationEmail(payload.email, payload.name, payload.password)
-    )
-  } else {
-    await res.send(400)
-  }
+    const payload = req.body.payload;
+    if (payload.email) {
+        await res.send(
+            await controller.sendRegistrationEmail(
+                payload.email,
+                payload.name,
+                payload.password
+            )
+        );
+    } else {
+        await res.send(400);
+    }
 });
 
 router.post('/newpassword', async (req, res) => {
-  const payload = req.body.payload;
-  if (payload.email) {
-    await res.send(
-      await controller.sendNewPassword(payload.email, payload.name, payload.password)
-    )
-  } else {
-    await res.send(400)
-  }
+    const payload = req.body.payload;
+    if (payload.email) {
+        await res.send(
+            await controller.sendNewPassword(
+                payload.email,
+                payload.name,
+                payload.password
+            )
+        );
+    } else {
+        await res.send(400);
+    }
 });
 
 router.post('/updatedwarningstatus', async (req, res) => {
-  const payload = req.body.payload;
-  if (payload.email) {
-    await res.send(
-      await controller.updatedWarningStatus(payload.email, payload.title, payload.status, payload.comment)
-    )
-  } else {
-    await res.send(400)
-  }
+    const payload = req.body.payload;
+    if (payload.email) {
+        await res.send(
+            await controller.updatedWarningStatus(
+                payload.email,
+                payload.title,
+                payload.status,
+                payload.comment
+            )
+        );
+    } else {
+        await res.send(400);
+    }
 });
 
 router.post('/newcomment', async (req, res) => {
-  const payload = req.body.payload;
-  if (payload.email) {
-    await res.send(
-      await controller.newWarningStatus(payload.email, payload.title, payload.name, payload.comment)
-    )
-  } else {
-    await res.send(400)
-  }
+    const payload = req.body.payload;
+    if (payload.email) {
+        await res.send(
+            await controller.newWarningStatus(
+                payload.email,
+                payload.title,
+                payload.name,
+                payload.comment
+            )
+        );
+    } else {
+        await res.send(400);
+    }
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 
-app.use('/email', require('./src/routes/email'));
-app.use('/sms', require('./src/routes/sms'));
+app.use('/email', require('./routes/email'));
+app.use('/sms', require('./routes/sms'));
 
 app.listen(8080, () => console.log('listening on port 8080'));
