6e1de0bd8c74fb49240b92d906c352cf523a17c4
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-18 13:58:02 +0100

    fixed

--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const morgan = require('morgan');
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
 
+// to parse json
+app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+app.get('/metrics', async (req, res) => {
+    await res.set('Content-Type', client.register.contentType);
+    await res.end(client.register.metrics());
+});
 
 app.use('/email', require('./routes/email'));
 app.use('/sms', require('./routes/sms'));
