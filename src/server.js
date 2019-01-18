5c034cc53aa0cdb533b71b8b182d23ea6433541a
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-18 13:54:20 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
 app.use('/email', require('./routes/email'));
 app.use('/sms', require('./routes/sms'));
 
