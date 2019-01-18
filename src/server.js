434fd4425c6a88abb29fbd81911bc43ceffeb97c
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-18 14:07:26 +0100

    addaspojødoikshajgkøsjod.ølhsløosajvøaldwhøkfjlvaghj,oipsodfhjkgfdkhaløjg

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const client = require('prom-client');
 const collectDefaultMetrics = client.collectDefaultMetrics;
